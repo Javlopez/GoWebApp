c52955f22cbd7d03001f08ce53d750e0e5f4da87
Javier Lopez Lopez <sjavierlopez@gmail.com>
2017-11-06 21:35:51 -0600

    Adding contentTypes

--- main/main.go
+++ main/main.go
@@ -3,12 +3,11 @@ package main
 import (
 	"net/http"
 	"io/ioutil"
+	"strings"
 )
 
 func main(){
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		writer.Write([]byte("Hello World"))
-	})
+	http.Handle("/", new(MyHandler))
 
 	http.ListenAndServe(":8000", nil)
 }
@@ -23,6 +22,21 @@ func (this *MyHandler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
 	data, err := ioutil.ReadFile(string(path))
 
 	if err ==  nil {
+		var contentType string
+
+		if strings.HasSuffix(path, ".css") {
+			contentType = "text/css"
+		} else if strings.HasSuffix(path, ".html") {
+			contentType = "text/html"
+		} else if strings.HasSuffix(path, ".js") {
+			contentType = "text/javascript"
+		} else if strings.HasSuffix(path, ".png") {
+			contentType = "image/png"
+		} else {
+			contentType = "text/plain"
+		}
+
+		w.Header().Add("Content-Type", contentType)
 		w.Write(data)
 	} else {
 		w.WriteHeader(404)
