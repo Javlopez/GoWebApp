70925ae91821993b3702cd9ffadd54dc0a3374fd
Javier Lopez Lopez <sjavierlopez@gmail.com>
2017-11-06 21:55:38 -0600

    Replacing by goSolution

--- main/main.go
+++ main/main.go
@@ -3,51 +3,12 @@ package main
 import (
 	"net/http"
 	//"io/ioutil"
-	"strings"
-	"os"
-	"bufio"
+	//"strings"
+	//"os"
+	//"bufio"
 )
 
 func main(){
-	http.Handle("/", new(MyHandler))
 
-	http.ListenAndServe(":8000", nil)
-}
-
-type MyHandler struct {
-	http.Handler
-}
-
-func (this *MyHandler) ServeHTTP(w http.ResponseWriter, request *http.Request) {
-
-	path := "public/" + request.URL.Path
-	f, err := os.Open(path)
-	//data, err := ioutil.ReadFile(string(path))
-
-	if err ==  nil {
-
-		bufferedReader := bufio.NewReader(f)
-		var contentType string
-
-		if strings.HasSuffix(path, ".css") {
-			contentType = "text/css"
-		} else if strings.HasSuffix(path, ".html") {
-			contentType = "text/html"
-		} else if strings.HasSuffix(path, ".js") {
-			contentType = "text/javascript"
-		} else if strings.HasSuffix(path, ".png") {
-			contentType = "image/png"
-		} else if strings.HasSuffix(path, ".mp4") {
-			contentType = "video/mp4"
-		} else {
-			contentType = "text/plain"
-		}
-
-		w.Header().Add("Content-Type", contentType)
-		bufferedReader.WriteTo(w)
-		//w.Write(data)
-	} else {
-		w.WriteHeader(404)
-		w.Write([]byte("404 - " + http.StatusText(404)))
-	}
+	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
 }
